873fc665db2728447af845c2ab2aeb9739a5f30d
Alec Hajinoff <hajinoff@gmail.com>
2026-05-18 14:54:46 +0100

    18/5/26, 14:54

--- React/src/NavigationBar.js
+++ React/src/NavigationBar.js
@@ -19,26 +19,33 @@ function NavigationBar({ isAuthenticated, isLoading, userRole }) {
   return (
     <nav>
       <div className="container">
-        <div className="nav-buttons">
-          {!isHomePage && (
-            <Link to="/" className="nav-link btn-text">
-              Home
-            </Link>
-          )}
-          <Link to="/Aboutme" className="nav-link btn-text">
-            About me
-          </Link>
-          <Link to="/Portfolio" className="nav-link btn-text">
-            Portfolio
-          </Link>
-          {!isUserDashboard &&
-            !isAdminDashboard &&
-            !isLoading &&
-            isAuthenticated && (
-              <Link to={getDashboardLink()} className="nav-link btn-text">
-                Dashboard
+        <div className="row align-items-center">
+          <div className="col-12 col-md-9">
+            <div className="nav-buttons">
+              {!isHomePage && (
+                <Link to="/" className="nav-link btn-text">
+                  Home
+                </Link>
+              )}
+              <Link to="/Aboutme" className="nav-link btn-text">
+                About me
               </Link>
-            )}
+              <Link to="/Portfolio" className="nav-link btn-text">
+                Portfolio
+              </Link>
+              {!isUserDashboard &&
+                !isAdminDashboard &&
+                !isLoading &&
+                isAuthenticated && (
+                  <Link to={getDashboardLink()} className="nav-link btn-text">
+                    Dashboard
+                  </Link>
+                )}
+            </div>
+          </div>
+          <div className="col-12 col-md-3">
+            {/* An empty column to match the header layout */}
+          </div>
         </div>
       </div>
     </nav>
