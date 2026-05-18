8951039d0fb81c0cb45aee79c7b4abb16a0c2ed1
Alec Hajinoff <hajinoff@gmail.com>
2026-05-18 14:37:49 +0100

    18/5/26, 14:37

--- React/src/Header.js
+++ React/src/Header.js
@@ -6,23 +6,25 @@ import "./Header.css";
 
 function Header({ isAuthenticated, isLoading, onLogoutComplete }) {
   return (
-    <div className="container">
-      <div className="row align-items-center">
-        <div className="col-12 col-md-9">
-          <Link to="/">
-            <img
-              id="logo"
-              src={blue}
-              alt="A company logo"
-              title="A company logo"
-            />
-          </Link>
-        </div>
+    <div className="header-wrapper">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-12 col-md-9">
+            <Link to="/">
+              <img
+                id="logo"
+                src={blue}
+                alt="A company logo"
+                title="A company logo"
+              />
+            </Link>
+          </div>
 
-        <div className="col-12 col-md-3 text-end">
-          {!isLoading && isAuthenticated && (
-            <LogoutComponent onLogoutComplete={onLogoutComplete} />
-          )}
+          <div className="col-12 col-md-3 text-end">
+            {!isLoading && isAuthenticated && (
+              <LogoutComponent onLogoutComplete={onLogoutComplete} />
+            )}
+          </div>
         </div>
       </div>
     </div>
