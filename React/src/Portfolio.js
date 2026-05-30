b0d6907760a87285c5efcb3f78bef9f2486ccbf0
Alec Hajinoff <hajinoff@gmail.com>
2026-05-30 07:28:27 +0100

    30/5/26, 7:28

--- React/src/Portfolio.js
+++ React/src/Portfolio.js
@@ -3,18 +3,18 @@ import "./Portfolio.css";
 
 function Portfolio() {
   return (
-    <div className="portfolio-container">
+    <div className="portfolio-container container">
       <div className="row justify-content-center">
-      <div className="col-12 col-lg-9">
-        <section className="hero">
-          <h2 className="hero-title">A practical overview of recent work</h2>
-        </section>
+        <div className="col-12 col-lg-9">
+          <section className="hero">
+            <h2 className="hero-title">A practical overview of recent work</h2>
+          </section>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-      </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </div>
     </div>
   );
