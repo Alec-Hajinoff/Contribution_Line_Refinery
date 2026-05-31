f426167551bc2c977dcfb93978ec5f3f9f3ad485
Alec Hajinoff <hajinoff@gmail.com>
2026-05-31 06:23:34 +0100

    31/5/26, 6:23

--- React/src/App.js
+++ React/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { BrowserRouter as Router, useLocation } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 import Header from "./Header";
 import NavigationBar from "./NavigationBar";
 import Footer from "./Footer";
@@ -9,7 +10,7 @@ import { checkSession } from "./ApiService";
 function AppContent() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [userRole, setUserRole] = useState(null); 
+  const [userRole, setUserRole] = useState(null);
   const location = useLocation();
 
   const verifySession = useCallback(async () => {
@@ -52,6 +53,7 @@ function AppContent() {
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <AppContent />
     </Router>
   );
