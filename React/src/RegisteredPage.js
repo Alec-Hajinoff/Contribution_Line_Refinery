dd0926cf86dd899baf1de382d9d3b81e9fa4effb
Alec Hajinoff <hajinoff@gmail.com>
2026-06-01 06:02:59 +0100

    1/6/26, 6:02

--- React/src/RegisteredPage.js
+++ React/src/RegisteredPage.js
@@ -5,15 +5,18 @@ import UserLogin from "./UserLogin.js";
 function RegisteredPage() {
   return (
     <div className="registered-page-wrapper">
-      <div className="container text-center">
+      <div className="container">
         <div className="row">
-          <div className="col-12 col-md-9">
-            <p>
+          <div className="col-12 text-center my-4">
+            <p className="w-100 whitespace-nowrap">
               Thank you for verifying your email address! Please log in using
               your credentials.
             </p>
           </div>
-          <div className="col-12 col-md-3">
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-3">
             <p className="registered-section-divider">Registered user login:</p>
             <UserLogin />
           </div>
